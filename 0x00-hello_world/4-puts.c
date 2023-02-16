ca29f2a45747087a87383d090d817cecef8f46ae
kabugi-marvin <kabugimarvin@gmail.com>
2023-02-16 23:26:18 +0300

    update1 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle");
-
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
