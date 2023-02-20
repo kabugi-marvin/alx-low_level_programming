6c22c033caed8b38fa0dabc49e97e0cda7616f57
kabugi-marvin <kabugimarvin@gmail.com>
2023-02-20 18:18:36 +0300

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int c;
+
+	c = 0;
+	while (c <= 9)
+	{
+		putchar(c + '0');
+		putchar(44);
+		putchar(32);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
