5847316e4419fb633fba0eebf3e000fe62d05583
kabugi-marvin <kabugimarvin@gmail.com>
2023-02-21 16:37:02 +0300

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints all alphabets
+ *
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
