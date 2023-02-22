6e351d5f4c09427c500f11d1154ed6a228b2dcf3
kabugi-marvin <kabugimarvin@gmail.com>
2023-02-22 16:12:26 +0300

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+
+/**
+ * _abs - function returns the absolute value
+ * @x: number to be converted to the absolute form
+ *
+ * Return: x in absolute form
+ */
+
+int _abs(int x)
+{
+	return (abs(x));
+}
