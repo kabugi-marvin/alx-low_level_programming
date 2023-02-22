f071e41574b6405ac6dd53cf4a7bfdaed2f65e08
kabugi-marvin <kabugimarvin@gmail.com>
2023-02-22 19:07:56 +0300

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+/**
+ * add - adds two integers and returns the results
+ * @a: integer arguement to be passed
+ * @b: integer arguement to be passed
+ *
+ * Return: Sum of two values
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
