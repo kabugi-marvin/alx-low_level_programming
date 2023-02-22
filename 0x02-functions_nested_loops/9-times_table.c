581acb2ca2bc1c95e53730dd68f1edcf470926f6
kabugi-marvin <kabugimarvin@gmail.com>
2023-02-22 18:57:32 +0300

    update1 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,16 +18,26 @@ void times_table(void)
 			result = i * j;
 			if (result < 10)
 			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar('0' + result);
+				if ((j == 0) && (result == 0))
+				{
+					_putchar('0' + result);
+				}
+				else
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + result);
+				}
 			}
-			else
+			else if (result > 9)
 			{
 				_putchar(' ');
 				_putchar('0' + result / 10);
 				_putchar('0' + result % 10);
 			}
+			else
+				_putchar('0' + result);
+
 			if (j < 9)
 				_putchar(',');
 		}
