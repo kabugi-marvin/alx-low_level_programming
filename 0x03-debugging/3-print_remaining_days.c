45a796ae6ca7283cafde2f6a3ee38079519787cd
kabugi-marvin <kabugimarvin@gmail.com>
2023-02-23 16:09:43 +0300

    corrected 3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -14,13 +14,13 @@ void print_remaining_days(int month, int day, int year)
 {
 	if (year % 4 == 0)
 	{
-		if ((year % 100 != 0) && month >= 2 && day >= 59)
+		if ((year % 100 != 0) && month >2 && day >= 59)
 		{
 			day++;
 		}
 		if (year % 100 == 0 && year % 400 == 0)
 		{
-			if (month >= 2 && day >= 59)
+			if (month > 2 && day >= 59)
 			{
 				day++;
 			}
