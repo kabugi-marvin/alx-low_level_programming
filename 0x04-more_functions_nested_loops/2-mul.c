0477f20b755a04915b311bb30e48ba1c1896f12c
kabugi-marvin <kabugimarvin@gmail.com>
2023-02-23 20:30:43 +0300

    added 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: integer parameter
+ * @b: integer parameter
+ *
+ * Return: Integer
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
