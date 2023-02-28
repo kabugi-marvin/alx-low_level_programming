41df82aa65e51e37649d1dc199c623446bb48300
kabugi-marvin <kabugimarvin@gmail.com>
2023-02-28 14:48:22 +0300

    added 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+ #include <string.h>
+
+/**
+ * _strlen -  returns the length of a string.
+ * @s: address of string parameter
+ *
+ * Return: Integer
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
