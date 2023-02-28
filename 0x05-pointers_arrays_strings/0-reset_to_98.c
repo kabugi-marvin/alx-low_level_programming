bc51d39a5f67ba965903f6ac29a18522c94c9675
kabugi-marvin <kabugimarvin@gmail.com>
2023-02-28 09:42:04 +0300

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+/**
+ * reset_to_98 - takes a pointer to an int as
+ * parameter and updates the value it points to to 98
+ * @n: address to be passed
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
