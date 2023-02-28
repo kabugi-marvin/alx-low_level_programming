582e42643fcb39dea6a2aaff4f7f47c6fa6c08c2
kabugi-marvin <kabugimarvin@gmail.com>
2023-02-28 15:01:47 +0300

    added 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * _puts -  prints a string, followed by a new line, to stdout.
+ * @str: argurment to be passed
+ *
+ * Return: Void
+ */
+void _puts(char *str)
+{
+	(puts(str));
+}
