c65771a3d25d7edc4a3d248fdf8600654caf38d9
kabugi-marvin <kabugimarvin@gmail.com>
2023-03-02 15:52:09 +0300

    added 3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include <string.h>
+
+/**
+ * _strcmp -  compares two strings.
+ * @s1: string to be compared
+ * @s2: string to be compared
+ *
+ * Return: signed integer depending on comparison
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int length;
+	int length2;
+
+	length = strlen(s1);
+	length2 = strlen(s2);
+	if (length == length2)
+		return (0);
+	else if (length < length2)
+		return (-15);
+	else
+		return (15);
+}
